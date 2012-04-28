bf6aec3ec8156684101c7caeeda9d8530c2df2f8
Mike Sheldon <elleo@gnu.org>
2012-04-28 21:31:29 +0100

    Remove debug line

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
 	HiScore hiscore;
 	view.rootContext()->setContextProperty("hiscore", &hiscore);
 	view.setSource(QUrl::fromLocalFile(DATADIR "/seagullstrike/seagullstrike.qml"));
-//	view.setSource(QUrl::fromLocalFile("seagullstrike.qml"));
 
 	view.showFullScreen();
 	return app.exec();
